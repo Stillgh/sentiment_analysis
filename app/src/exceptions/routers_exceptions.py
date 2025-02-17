daf8b62fe2afdbcb9d8ba3f563cf753767feba27
Vladimir Neumolotov <neumolotov.v@gmail.com>
2025-02-17 16:05:18 +0400

    added admin_required page

--- app/src/exceptions/routers_exceptions.py
+++
@@ -1,8 +0,0 @@
-from fastapi import HTTPException
-from starlette import status
-
-admin_exception = HTTPException(
-    status_code=status.HTTP_403_FORBIDDEN,
-    detail="To access that page you should be admin."
-)
-
--- app/src/routes/admin_router.py
+++ app/src/routes/admin_router.py
@@ -9,7 +9,6 @@ from config.auth_config import get_auth_settings
 from database.database import get_session
 from entities.auth.auth_entities import TokenData
 from entities.user.user_role import UserRole
-from exceptions.routers_exceptions import admin_exception
 from routes.home_router import templates
 from service.auth.auth_service import authenticate_cookie, get_current_active_user
 from service.crud.user_service import get_all_users
@@ -27,7 +26,7 @@ async def admin_panel(
 ):
     admin_user = await get_current_active_user(token, session)
     if admin_user.role != UserRole.ADMIN:
-        raise admin_exception
+        return templates.TemplateResponse("admin_required.html", {"request": request})
     return templates.TemplateResponse("admin.html", {"request": request})
 
 
@@ -39,7 +38,16 @@ async def get_users(
     user = await get_current_active_user(token, session)
 
     if not user or user.role != UserRole.ADMIN:
-        raise admin_exception
+        return templates.TemplateResponse("admin_required.html", {"request": request})
     all_users = list(map(user_to_user_dto, get_all_users(session)))
     return templates.TemplateResponse("admin_users.html", {"request": request, "users": all_users})
 
+
+@admin_router.get("/admin_required", response_class=HTMLResponse)
+async def admin_required(request: Request,
+                         token: Annotated[TokenData, Depends(authenticate_cookie)],
+                         session: Annotated[Session, Depends(get_session)]):
+    user = await get_current_active_user(token, session)
+    if not user or user.role != UserRole.ADMIN:
+        return templates.TemplateResponse("admin_required.html", {"request": request})
+    return templates.TemplateResponse("admin.html", {"request": request})
